532f424bcb36447e3e09f1921485d3e6072d7f5a
Alexandre <alexandremarmen@gmail.com>
2025-02-10 18:00:38 -0500

    terra preview

--- my-portfolio/src/components/web-specialty.js
+++ my-portfolio/src/components/web-specialty.js
@@ -17,10 +17,14 @@ const style = {
 export default function Web({ interestsList, toggleInterest }) {
     const isAdded = (interest) => interestsList.includes(interest);
     const [open, setOpen] = React.useState(false);
+    const [openPreview, setOpenPreview] = React.useState(false);
+    const handleOpenPreview = () => setOpenPreview(true);
+    const handleClosePreview = () => setOpenPreview(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
   return (
     <div>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=visibility" />
         <h1 className="speciality-title">Web Development Technical Skills</h1>
         <div className="skills-container">
             <div>
@@ -107,9 +111,10 @@ export default function Web({ interestsList, toggleInterest }) {
                             <path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"/>
                             </svg>
                     </button> */}
-                <Button sx={{color: 'white', textTransform: "capitalize"}} onClick={handleOpen} className="rl-btn">
-                    See Recommendation <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" style={{marginLeft: "8px"}} fill="#e8eaed"><path d="M320-160q-33 0-56.5-23.5T240-240v-120h120v-90q-35-2-66.5-15.5T236-506v-44h-46L60-680q36-46 89-65t107-19q27 0 52.5 4t51.5 15v-55h480v520q0 50-35 85t-85 35H320Zm120-200h240v80q0 17 11.5 28.5T720-240q17 0 28.5-11.5T760-280v-440H440v24l240 240v56h-56L510-514l-8 8q-14 14-29.5 25T440-464v104ZM224-630h92v86q12 8 25 11t27 3q23 0 41.5-7t36.5-25l8-8-56-56q-29-29-65-43.5T256-684q-20 0-38 3t-36 9l42 42Zm376 350H320v40h286q-3-9-4.5-19t-1.5-21Zm-280 40v-40 40Z"/></svg>
-                    </Button>
+                <div sx={{color: 'white', textTransform: "capitalize"}} onClick={handleOpen} className="rl-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="28px" fill="#e8eaed"><path d="M320-160q-33 0-56.5-23.5T240-240v-120h120v-90q-35-2-66.5-15.5T236-506v-44h-46L60-680q36-46 89-65t107-19q27 0 52.5 4t51.5 15v-55h480v520q0 50-35 85t-85 35H320Zm120-200h240v80q0 17 11.5 28.5T720-240q17 0 28.5-11.5T760-280v-440H440v24l240 240v56h-56L510-514l-8 8q-14 14-29.5 25T440-464v104ZM224-630h92v86q12 8 25 11t27 3q23 0 41.5-7t36.5-25l8-8-56-56q-29-29-65-43.5T256-684q-20 0-38 3t-36 9l42 42Zm376 350H320v40h286q-3-9-4.5-19t-1.5-21Zm-280 40v-40 40Z"/></svg>
+                    <span style={{minWidth:"200px"}} >Recommandation Letter</span> 
+                    </div>
                     <Modal
                         open={open}
                         onClose={handleClose}
@@ -125,6 +130,73 @@ export default function Web({ interestsList, toggleInterest }) {
                 </div>
 
             </div>
+
+
+            {/*Hackathon*/}
+            <div className="experience-item">
+                
+                <div className="logo-placeholder w-20 h-20 flex items-center justify-center rounded">
+                    <img src="static/images/PolyHx-logo.png" alt="logo" className="w-16 h-16 max-w-none" style={{borderRadius: "8px"}} />
+                </div>
+                {/* Experience Details */}
+                <div className="experience-details">
+                    <h2 className="text-xl font-semibold text-white">
+                        PolyHacks Hackathon 2025 | 2nd Place
+                    </h2>
+                    <p className="italic text-gray-400">
+                    Fullstack Developer – Participant | February 2024 | Montréal, QC
+                    </p>
+                    <ul className="mt-4 list-disc list-inside text-gray-300 space-y-2">
+                    <li>
+                        Designed an interactive educational web application in 24h regarding sustainable issues.
+                    </li>
+                    <li>
+                        Integrated AI feedback and chatbot to deepen user knowledge on certain topics.
+                    </li>
+                    <li>
+                        Leveraged technologies such as Angular, TypeScript, NodeJS, and ThreeJs.
+                    </li>
+                    <li>
+                        Achieved 2nd place in my second-ever hackathon experience.
+                    </li>
+                    </ul>
+                </div>
+                <div className="experience-footer">
+                    {/* <button onClick={() => toggleInterest("Ultrapark Web Software Developer Intern")}> 
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill={isAdded("Ultrapark Web Software Developer Intern") ? "#f6ff00" : "#ffffff"}>
+                            <path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"/>
+                            </svg>
+                    </button> */}
+                    <div sx={{color: 'white', textTransform: "capitalize"}} onClick={handleOpenPreview} className="rl-btn">
+                        <span className="material-symbols-outlined">
+                        visibility
+                        </span>
+                        Preview
+                    </div>
+                    <Modal
+                        open={openPreview}
+                        onClose={handleClosePreview}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <img src="static/images/terra.gif" alt="r-l" className="rl-img" style={{borderRadius: "4px"}} />
+                        </Box>
+                    </Modal>
+                    <a href="https://github.com/m4rmen/polyhx2k25" >
+                    <div sx={{color: 'white', textTransform: "capitalize"}} className="rl-btn">
+                        <i className="fa fa-github-square"   style={{fontSize: "36px"}}></i>
+                        <span style={{minWidth:"100px", marginTop: "6px"}}>
+                            See project
+                        </span>
+                    </div>
+                    </a>
+                </div>
+
+
+            </div>
+
+
 
             {/*Projet*/}
             <div className="experience-item">
